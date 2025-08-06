ab05b3ce1ccc3eb6af85d2adf556bda19f5ef410
Leandro M <leandro.maldonado.dev@gmail.com>
2025-08-06 13:05:03 -0500

    update paths

--- apps/blog-be/src/middleware/security.ts
+++ apps/blog-be/src/middleware/security.ts
@@ -239,7 +239,7 @@ export const detectSuspiciousActivity = (
  */
 export const getCorsOptions = () => {
   const allowedOrigins = process.env.NODE_ENV === 'production'
-    ? ['https://ng-corner.dev', 'https://www.ng-corner.dev'] // Production domains
+    ? ['https://ng-corner.dev', 'https://www.ng-corner.dev', 'https://www.ng-corner.com/','http://staging.ng-corner.com/'] // Production domains
     : ['http://localhost:4200', 'http://127.0.0.1:4200']; // Development
 
   return {
--- apps/blog/src/server.ts
+++ apps/blog/src/server.ts
@@ -7,6 +7,7 @@ import {
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
@@ -25,6 +26,14 @@ const angularApp = new AngularNodeAppEngine();
  * });
  * ```
  */
+
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: process.env['API_URL'],
+    changeOrigin: true
+  })
+);
 
 /**
  * Serve static files from /browser
